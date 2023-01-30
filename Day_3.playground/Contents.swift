f47836a21a34379ee5fa40b16f89096855e9b05f
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-01-30 22:16:52 +0100

    DAY_3_Finish - I addded a summrize.

--- Day_3.playground/Contents.swift
+++ Day_3.playground/Contents.swift
@@ -26,6 +26,7 @@ print("There are \(weeks) weeks and \(days) days until the event.")
 
 let number = 465
 let isMultiple = number.isMultiple(of: 7) // is divisible by 7?
+// is Multiple (jest wielokrotnością?)
 
 //MARK: Operator overloading
 
@@ -152,3 +153,13 @@ case 50..<85:
 default:
     print("You did great!")
 }
+
+//MARK: Summarize of day 3 - What have I learned?
+/*
+1. Swift has operators for doing arithmetic and for comparison; they mostly work like you already know.
+2. There are compound variants of arithmetic operators that modify their variables in place: +=, -=, and so on.
+3. You can use if, else, and else if to run code based on the result of a condition.
+4. Swift has a ternary operator that combines a check with true and false code blocks. Although you might see it in other code, I wouldn’t recommend using it yourself.
+5. If you have multiple conditions using the same value, it’s often clearer to use switch instead.
+6. You can make ranges using ..< and ... depending on whether the last number should be excluded or included.
+*/
