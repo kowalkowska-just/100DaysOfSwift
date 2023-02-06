95297429191c96c0eaf01f1f46d030cec870257c
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-02-06 21:27:25 +0100

    DAY-4-Exiting Loops

--- Day_4_Loops.playground/Contents.swift
+++ Day_4_Loops.playground/Contents.swift
@@ -58,3 +58,20 @@ repeat {
 
 //MARK: EXITING LOOPS
 
+var countDown = 10
+
+while countDown >= 0 {
+    print(countDown)
+    countDown -= 1
+}
+
+print("Blast off!")
+
+while countDown >= 0 {
+    print(countDown)
+    
+    if countDown == 4 {
+        print("I'm bored. Let's go now")
+        break // exit the function
+    }
+}
