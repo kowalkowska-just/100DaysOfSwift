72fae71dfeb327aab0b9a549c92897643e3ea63a
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-02-06 21:39:44 +0100

    DAY-4- Exiting multiple loops

--- Day_4_Loops.playground/Contents.swift
+++ Day_4_Loops.playground/Contents.swift
@@ -75,3 +75,27 @@ while countDown >= 0 {
         break // exit the function
     }
 }
+
+//MARK: - EXITING MULTIPLE LOOPS
+/* If you put a loop inside a loop it’s called a nested loop, and it’s not uncommon to want to break out of both the inner loop and the outer loop at the same time.
+*/
+
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print("\(i) * \(j) is \(product)")
+    }
+}
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print("\(i) * \(j) is \(product)")
+        
+        if product == 50 {
+            print("It's a bullseye")
+            break outerLoop
+        }
+    }
+}
