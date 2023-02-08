1be7432b1ee3d3c4cca3a14e7eb2fe6cd918a20b
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-02-08 20:49:35 +0100

    DAY_5_Introduction to functions.

--- Day_5_Functions_parametrs_and_errors.playground/Contents.swift
+++ Day_5_Functions_parametrs_and_errors.playground/Contents.swift
@@ -1,2 +1,19 @@
 import UIKit
 
+//MARK: - Functions
+
+/* Swift functions start with the func keyword, then your function name, then open and close parentheses. All the body of your function – the code that should be run when the function is requested – is placed inside braces.
+*/
+
+func printHelp() {
+    let message = """
+Welcome to MyApp!
+
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+
+    print(message)
+}
+printHelp()
+
