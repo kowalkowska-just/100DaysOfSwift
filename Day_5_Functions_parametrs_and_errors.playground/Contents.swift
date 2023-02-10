8208d6dee7c98a7c7681befb1edcd7135b670551
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-02-10 21:18:01 +0100

    DAY_5_Returing values.

--- Day_5_Functions_parametrs_and_errors.playground/Contents.swift
+++ Day_5_Functions_parametrs_and_errors.playground/Contents.swift
@@ -26,3 +26,17 @@ func square(number: Int) {
 }
 
 square(number: 5)
+
+//MARK: - Returning values
+/* As well as receiving data, functions can also send back data. To do this, write a dash then a right angle bracket after your function’s parameter list, then tell Swift what kind of data will be returned.
+ 
+ Inside your function, you use the return keyword to send a value back if you have one. Your function then immediately exits, sending back that value – no other code from that function will be run. */
+ 
+ func squareReturn(number: Int) -> Int {
+    return number * number
+}
+
+let result = squareReturn(number: 8)
+print(result)
+
+
