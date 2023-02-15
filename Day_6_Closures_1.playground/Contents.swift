d0e2a0af0a71ba76f2bb4f5570365aeb55343a86
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-02-15 20:17:50 +0100

    Day_6_Accepting partameters in a closure.

--- Day_6_Closures_1.playground/Contents.swift
+++ Day_6_Closures_1.playground/Contents.swift
@@ -10,3 +10,12 @@ let driving = {
 
 driving()
 
+//MARK: - Accepting partameters in a closure.
+
+let driving1 = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+driving1("London")
+
+/* To make a closure accept parameters, list them inside parentheses just after the opening brace, then write in so that Swift knows the main body of the closure is starting. */
+
