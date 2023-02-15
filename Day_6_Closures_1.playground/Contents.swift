ed31d38caeb3a035804132e3b50f58b3977d143e
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-02-15 20:06:05 +0100

    Day_6_Creating basic closures.

---
+++ Day_6_Closures_1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//MARK: - Creating basic closures.
+
+let driving = {
+    print("I'm driving in my car")
+}
+
+/* That effectively creates a function without a name, and assigns that function to driving. You can now call driving() as if it were a regular function, like this: */
+
+driving()
+
