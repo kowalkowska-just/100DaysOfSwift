0001e5bf499279280ceac100fcdec9fbb23d94de
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-02-18 18:19:55 +0100

    Day_7_Using closures as parameters when they return values.

--- Day_7_Closures_2.playground/Contents.swift
+++ Day_7_Closures_2.playground/Contents.swift
@@ -11,3 +11,17 @@ func travel(action: (String) -> Void) {
 travel { (place: String) in
     print("I'm going to \(place) in my car")
 }
+
+
+//MARK: - Using closures as parameters when they return values.
+
+func travel(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
