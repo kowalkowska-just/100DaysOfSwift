305019debc20b267fae727450f53925920a18f39
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-02-22 23:13:39 +0100

    Day_8_Diffrence between a struc and a tuple

--- Day_8_Structs_1.playground/Contents.swift
+++ Day_8_Structs_1.playground/Contents.swift
@@ -2,9 +2,9 @@ import UIKit
 
 //MARK: - Creating struct
 
-struct Sport {
-    var name: String
-}
+//struct Sport {
+//    var name: String
+//}
 
 var tennis = Sport(name: "Tennis")
 print(tennis.name)
@@ -12,3 +12,23 @@ print(tennis.name)
 tennis.name = "Lawn tennis"
 
 print(tennis.name)
+
+//MARK: - Diffrence between a struct and a tuple.
+
+struct User {
+    var name: String
+    var age: Int
+    var city: String
+}
+
+// using a struct in function
+func authenticate(_ user: User) { }
+func showProfile(for user: User) { }
+func signOut(_ user: User) { }
+
+
+//using a tuple in function
+func authenticate(_ user: (name: String, age: Int, city: String)) { }
+func showProfile(for user: (name: String, age: Int, city: String)) { }
+func signOut(_ user: (name: String, age: Int, city: String)) { }
+
